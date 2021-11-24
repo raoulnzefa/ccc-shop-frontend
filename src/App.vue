a3f089b4856a50cd5a861bf9b09dbdb301c9f7ae
1324 <1324.com>
2021-11-25 00:21:05 +0800

    add carousel, sign in form, and sign up form

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,55 @@
 <template>
   <v-app>
-    <v-app-bar
-      color="cyan darken-2 accent-4"
-    >
-      <v-toolbar-title>
-        CCC Shop
-      </v-toolbar-title>
+    <v-app-bar color="cyan darken-2 accent-4">
+      <v-toolbar-title> CCC Shop </v-toolbar-title>
 
       <v-form class="search-bar">
         <v-container>
           <v-row>
-            <v-text-field
-              label="搜尋商品"
-              outlined
-              dense
-            ></v-text-field>
+            <v-text-field label="搜尋商品" outlined dense></v-text-field>
             <v-btn icon>
               <v-icon>mdi-magnify</v-icon>
             </v-btn>
           </v-row>
         </v-container>
       </v-form>
-      
+
       <v-spacer></v-spacer>
 
-      <v-dialog
-      v-model="dialog"
-      width="500"
-      >
+      <!-- signInSignUpDialog -->
+      <v-dialog v-model="signInSignUpDialog" width="500">
         <v-card>
-          <v-card-title class="text-h5 cyan lighten-2">
-            登入 / 註冊
-          </v-card-title>
+          <v-card-title class="text-h5"> 登入 / 註冊 </v-card-title>
 
           <v-divider></v-divider>
 
+          <v-card-text style="padding:0px">
+            <v-carousel
+              :show-arrows="false"
+              cycle
+              height="250"
+              interval=4500
+              hide-delimiter-background
+              show-arrows-on-hover
+              delimiter-icon="mdi-minus"
+              style="padding:0px"
+            >
+              <v-carousel-item v-for="(slide, i) in slides" :key="i">
+                <v-sheet :color="colors[i]" height="100%">
+                  <v-row class="fill-height" align="center" justify="center">
+                    <div class="text-h2">{{ slide }} Slide</div>
+                  </v-row>
+                </v-sheet>
+              </v-carousel-item>
+            </v-carousel>
+          </v-card-text>
+
           <v-card-actions>
-            <v-row justify="center">
-              <v-btn
-              color="primary"
-              text
-              @click="dialog = false"
-              >
+            <v-row justify="center" style="margin:0px">
+              <v-btn color="primary" text @click="signUpDialog = !signUpDialog">
                 註冊
               </v-btn>
-              <v-btn
-                color="primary"
-                text
-                @click="dialog = false"
-              >
+              <v-btn color="primary" text @click="signInDialog = !signInDialog">
                 登入
               </v-btn>
             </v-row>
@@ -56,13 +57,152 @@
         </v-card>
       </v-dialog>
 
-      <v-btn plain>
-        我想賣東西
-      </v-btn>
+      <!-- signUpDialog -->
+      <v-dialog v-model="signUpDialog" width="500">
+        <v-card>
+          <v-card-title>
+            <span class="text-h5">Sign Up</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field
+                    label="Legal first name*"
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field
+                    label="Legal middle name"
+                    hint="example of helper text only on focus"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field
+                    label="Legal last name*"
+                    hint="example of persistent helper text"
+                    persistent-hint
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field label="Email*" required></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field
+                    label="Password*"
+                    type="password"
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6">
+                  <v-select
+                    :items="['0-17', '18-29', '30-54', '54+']"
+                    label="Age*"
+                    required
+                  ></v-select>
+                </v-col>
+                <v-col cols="12" sm="6">
+                  <v-autocomplete
+                    :items="[
+                      'Skiing',
+                      'Ice hockey',
+                      'Soccer',
+                      'Basketball',
+                      'Hockey',
+                      'Reading',
+                      'Writing',
+                      'Coding',
+                      'Basejump',
+                    ]"
+                    label="Interests"
+                    multiple
+                  ></v-autocomplete>
+                </v-col>
+              </v-row>
+            </v-container>
+            <small>*indicates required field</small>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" text @click="signUpDialog = false">
+              Close
+            </v-btn>
+            <v-btn
+              color="blue darken-1"
+              text
+              @click="(signUpDialog = false), (signInSignUpDialog = false)"
+            >
+              Save
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
 
-      <v-btn @click="dialog = true" plain>
-        登入/註冊
-      </v-btn>
+      <!-- signInDialog -->
+
+      <v-dialog v-model="signInDialog" width="500">
+        <v-card>
+          <v-card-title>
+            <span class="text-h5">Sign in</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field
+                    label="Legal first name*"
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field
+                    label="Legal middle name"
+                    hint="example of helper text only on focus"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field
+                    label="Legal last name*"
+                    hint="example of persistent helper text"
+                    persistent-hint
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field label="Email*" required></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field
+                    label="Password*"
+                    type="password"
+                    required
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+            </v-container>
+            <small>*indicates required field</small>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" text @click="signInDialog = false">
+              Close
+            </v-btn>
+            <v-btn
+              color="blue darken-1"
+              text
+              @click="(signInDialog = false), (signInSignUpDialog = false)"
+            >
+              Save
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
+      <v-btn plain> 我想賣東西 </v-btn>
+
+      <v-btn @click="signInSignUpDialog = true" plain> 登入/註冊 </v-btn>
 
       <v-btn icon>
         <v-icon>mdi-cart-outline</v-icon>
@@ -70,19 +210,29 @@
     </v-app-bar>
 
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
 export default {
-  name: 'App',
+  name: "App",
   components: {},
   data: () => {
     return {
-      dialog: false
-    }
+      signInSignUpDialog: false,
+      signInDialog: false,
+      signUpDialog: false,
+      colors: [
+        "indigo",
+        "warning",
+        "pink darken-2",
+        "red lighten-1",
+        "deep-purple accent-4",
+      ],
+      slides: ["First", "Second", "Third", "Fourth", "Fifth"],
+    };
   },
 };
 </script>
