74a9bef1234026f02a46a268d8f44bcc753fced8
Ricky Hu <ricky9667@gmail.com>
2021-11-24 21:22:14 +0800

    create appbar and its components

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,71 @@
 <template>
   <v-app>
     <v-app-bar
-      app
-      color="primary"
-      dark
+      color="cyan darken-2 accent-4"
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+      <v-toolbar-title>
+        CCC Shop
+      </v-toolbar-title>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+      <v-form class="search-bar">
+        <v-container>
+          <v-row>
+            <v-text-field
+              label="搜尋商品"
+              outlined
+              dense
+            ></v-text-field>
+            <v-btn icon>
+              <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+          </v-row>
+        </v-container>
+      </v-form>
+      
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+      <v-dialog
+      v-model="dialog"
+      width="500"
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        <v-card>
+          <v-card-title class="text-h5 cyan lighten-2">
+            登入 / 註冊
+          </v-card-title>
+
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-row justify="center">
+              <v-btn
+              color="primary"
+              text
+              @click="dialog = false"
+              >
+                註冊
+              </v-btn>
+              <v-btn
+                color="primary"
+                text
+                @click="dialog = false"
+              >
+                登入
+              </v-btn>
+            </v-row>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
+      <v-btn plain>
+        我想賣東西
+      </v-btn>
+
+      <v-btn @click="dialog = true" plain>
+        登入/註冊
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-cart-outline</v-icon>
       </v-btn>
     </v-app-bar>
 
@@ -44,12 +76,20 @@
 </template>
 
 <script>
-
 export default {
   name: 'App',
-
-  data: () => ({
-    //
-  }),
+  components: {},
+  data: () => {
+    return {
+      dialog: false
+    }
+  },
 };
 </script>
+
+<style>
+.search-bar {
+  margin-top: 24px;
+  margin-left: 20px;
+}
+</style>
