57fc6372607d71f98499d44287bb18efa37a9fd1
1324 <1324.com>
2021-11-30 17:42:10 +0800

    comment vuex store/index

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,9 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import login from "./modules/login"
+// import Vue from 'vue'
+// import Vuex from 'vuex'
+// import login from "./modules/login"
 
-Vue.use(Vuex)
+// Vue.use(Vuex)
 
-export default new Vuex.Store({
-  modules: { login },
-})
+// export default new Vuex.Store({
+//   modules: { login },
+// })
