e8698e44407e496d27678edf1142ed66653f21b1
lohsuan <angelalo88362@gmail.com>
2021-12-02 20:31:26 +0800

    add productStore in index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import userStore from "./modules/userStore"
+import productStore from "./modules/productStore"
 
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
-  modules: { userStore },
+  modules: { userStore, productStore },
 })
