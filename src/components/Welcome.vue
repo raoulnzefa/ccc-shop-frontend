cdcec1fcce36e7cf6cd5e40adeb646c466ef3ae6
lohsuan <1324.com>
2021-12-05 23:53:02 +0800

    Welcome component

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,18 +1,42 @@
 <template>
-<Welcome></Welcome>
+  <v-row class="text-center welcome-page">
+    <v-col cols="4">
+      <h1 class="font-weight-medium mt-5 ml-10 text-left">
+        <i>Welcome to CCC Shop</i>
+      </h1>
+      <h3 class="headline font-weight-regular ml-10 text-left">
+        <i>— Laugh out Loud =)</i>
+      </h3>
+    </v-col>
+    <v-col cols="8">
+      <v-carousel
+        cycle
+        height="250"
+        interval="6000"
+        hide-delimiter-background
+        delimiter-icon="mdi-minus"
+        style="margin-right: 0px"
+      >
+        <v-carousel-item
+          v-for="(item, i) in items"
+          :key="i"
+          :src="item.src"
+        ></v-carousel-item>
+      </v-carousel>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-import Welcome from "./Welcome.vue";
-
 export default {
-  name: "HelloWorld",
+  name: "Welcome",
   data: () => {
     return {
       items: [
         {
           src: "https://techsaydigi.com/wp-content/uploads/2021/10/c5482f525b8615658cc1a33d35ce0fed.jpg",
         },
+
         {
           src: "https://cdn2.ettoday.net/images/5953/5953731.jpg",
         },
@@ -25,7 +49,6 @@ export default {
       ],
     };
   },
-  components: { Welcome },
 };
 </script>
 
