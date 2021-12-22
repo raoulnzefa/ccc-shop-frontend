332789cf4a41a402dcf4996ea01ee56435c63811
lohsuan <1324.com>
2021-12-22 22:42:38 +0800

    implemented get current discount

---
+++ src/store/modules/discountStore.js
@@ -0,0 +1,70 @@
+import { getCurrentSeasoningsDiscount } from "../../api/discountApi";
+import { getCurrentSpecialDiscount } from "../../api/discountApi";
+import { getCurrentShippingDiscount } from "../../api/discountApi";
+
+const state = {
+    seasoningsDiscounts: [],
+    specialDiscounts: [],
+    shippingDiscounts: [],
+}
+//////// shippingDiscounts: []
+// discountCode
+// endTime
+// policyDescription
+// startTime
+// targetPrice
+// venderId
+//////////
+
+//////// seasoningsDiscountList: []
+// discountCode
+// discountRate
+// endTime
+// policyDescription
+// startTime
+// venderId
+//////////
+
+//////// specialDiscountList: []
+// venderId;
+// policyDescription;
+// startTime;
+// endTime;
+// category;
+// discountRate;
+//////////
+
+const getters = {}
+
+const actions = {
+    async loadCurrentSeasoningDiscount({ commit }) {
+        const seasoningsDiscounts = await getCurrentSeasoningsDiscount();
+        commit('loadCurrentSeasoningDiscountData', seasoningsDiscounts.seasoningsDiscountList);
+        console.log("seasoningsDiscounts.seasoningsDiscountList", seasoningsDiscounts.seasoningsDiscountList)
+    },
+    async loadCurrentSpecialDiscount({ commit }) {
+        const specialDiscounts = await getCurrentSpecialDiscount();
+        commit('loadCurrentSpecialDiscountData', specialDiscounts.specialDiscountList);
+        console.log("specialDiscounts.specialDiscountList", specialDiscounts.specialDiscountList)
+
+    },
+    async loadCurrentShippingDiscount({ commit }) {
+        const shippingDiscounts = await getCurrentShippingDiscount();
+        commit('loadCurrentShippingDiscountData', shippingDiscounts.shippingDiscountList);
+        console.log("shippingDiscounts.shippingDiscountList", shippingDiscounts.shippingDiscountList)
+    }
+}
+
+const mutations = {
+    loadCurrentSeasoningDiscountData(state, seasoningsDiscounts) {
+        state.seasoningsDiscounts = seasoningsDiscounts
+    },
+    loadCurrentSpecialDiscountData(state, specialDiscounts) {
+        state.specialDiscounts = specialDiscounts
+    },
+    loadCurrentShippingDiscountData(state, shippingDiscounts) {
+        state.shippingDiscounts = shippingDiscounts
+    }
+}
+
+export default { namespaced: true, state, getters, actions, mutations };
