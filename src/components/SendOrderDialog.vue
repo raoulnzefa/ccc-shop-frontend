35560c261a2b052a9268eec191bb42a7d9ad3494
Ricky Hu <ricky9667@gmail.com>
2021-12-29 17:25:13 +0800

    add order confirm dialog to shopping cart

---
+++ src/components/SendOrderDialog.vue
@@ -0,0 +1,141 @@
+<template>
+  <v-dialog
+      v-model="isOpenDialog"
+      fullscreen
+      hide-overlay
+      transition="dialog-bottom-transition"
+  >
+    <v-card>
+      <v-toolbar
+          dark
+          color="primary"
+      >
+        <v-btn
+            icon
+            dark
+            @click="closeDialog()"
+        >
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+        <v-toolbar-title>訂單確認</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items>
+          <v-btn
+              dark
+              text
+              @click="closeDialog()"
+          >
+            結帳
+          </v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+
+      <!-- shopping cart items list -->
+      <v-list three-line subheader class="px-8 py-4">
+        <v-subheader class="subtitle-1">訂單內容</v-subheader>
+
+        <v-list-item class="my-4">
+          <v-img
+              max-height="150"
+              max-width="150"
+              src="https://picsum.photos/id/11/500/300"
+          ></v-img>
+          <v-list-item-content class="ml-6">
+            <v-list-item-title>Macbook pro</v-list-item-title>
+            <v-list-item-subtitle>$54700 x 3 = $164100</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item class="my-4">
+          <v-img
+              max-height="150"
+              max-width="150"
+              src="https://picsum.photos/id/11/500/300"
+          ></v-img>
+          <v-list-item-content class="ml-6">
+            <v-list-item-title>Macbook pro</v-list-item-title>
+            <v-list-item-subtitle>$54700 x 3 = $164100</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <v-divider></v-divider>
+
+      <!-- payment settings -->
+      <v-list three-line subheader class="px-8 py-4">
+        <v-subheader>付款詳情</v-subheader>
+
+        <v-list-item>
+          <v-icon large>mdi-ticket-percent</v-icon>
+          <v-list-item-content class="ml-6">
+            <v-list-item-title>使用折扣</v-list-item-title>
+            <v-list-item-subtitle>未使用折扣</v-list-item-subtitle>
+          </v-list-item-content>
+          <v-spacer></v-spacer>
+          <v-btn icon>
+            <v-icon>mdi-pencil</v-icon>
+          </v-btn>
+        </v-list-item>
+
+        <v-list-item>
+          <v-icon large>mdi-credit-card</v-icon>
+          <v-list-item-content class="ml-6">
+            <v-list-item-title>付款方式</v-list-item-title>
+            <v-list-item-subtitle>信用卡 (9999888-7777666)</v-list-item-subtitle>
+          </v-list-item-content>
+          <v-spacer></v-spacer>
+          <v-btn icon>
+            <v-icon>mdi-pencil</v-icon>
+          </v-btn>
+        </v-list-item>
+
+        <v-list-item>
+          <v-icon large>mdi-truck-delivery</v-icon>
+          <v-list-item-content class="ml-6">
+            <v-list-item-title>寄送地址</v-list-item-title>
+            <v-list-item-subtitle>台北市大安區忠孝東路xxx號5F</v-list-item-subtitle>
+          </v-list-item-content>
+          <v-spacer></v-spacer>
+          <v-btn icon>
+            <v-icon>mdi-pencil</v-icon>
+          </v-btn>
+        </v-list-item>
+      </v-list>
+      <v-divider></v-divider>
+
+      <v-list subheader class="px-8 py-4">
+        <v-list-item>
+          <v-icon large>mdi-currency-usd</v-icon>
+          <v-list-item-content class="ml-6">
+            <v-list-item-title class="d-flex flex-row justify-space-between">
+              <div>總金額</div>
+              <div>$ 0</div>
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: "SendOrderDialog",
+  data: () => {
+    return {
+      isOpenDialog: false
+    }
+  },
+  methods: {
+    openDialog() {
+      this.isOpenDialog = true
+    },
+    closeDialog() {
+      this.isOpenDialog = false
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/ShoppingCartCard.vue
+++ src/components/ShoppingCartCard.vue
@@ -4,28 +4,45 @@
       <v-card class="py-4 px-8 my-4">
         <h3 class="text-center">{{ items.venderName }}</h3>
         <v-divider class="my-4"></v-divider>
-        <ShoppingCartItemRow v-for="item in items.items" :key="item.id" :item="item" />
+        <ShoppingCartItemRow v-for="item in items.items" :key="item.id" :item="item"/>
         <v-divider class="my-4"></v-divider>
         <v-row class="d-flex flex-row justify-space-between align-center ma-4">
           <h3>總金額：$ {{ shopTotalPrice }}</h3>
-          <v-btn color="cyan" class="white--text ma-5" @click="purchaseItemsFromShop()">
+          <v-btn
+              color="cyan"
+              class="white--text ma-5"
+              @click="$refs.sendOrderDialog.openDialog()"
+          >
             立即下單
           </v-btn>
         </v-row>
       </v-card>
     </v-list>
+
+    <SendOrderDialog
+        ref="sendOrderDialog"
+        identity="2"
+        @close="isOpenDialog = false"
+    />
   </v-container>
 </template>
 
 <script>
 import ShoppingCartItemRow from "./ShoppingCartItemRow";
+import SendOrderDialog from "./SendOrderDialog";
 
 export default {
   name: "ShoppingCartCard",
   components: {
+    SendOrderDialog,
     ShoppingCartItemRow
   },
   props: ["items"],
+  data: () => {
+    return {
+      isOpenDialog: false
+    }
+  },
   computed: {
     shopTotalPrice: function () {
       return this.$store.getters['shoppingCartStore/getShopTotalPrice'](this.items.venderName)
